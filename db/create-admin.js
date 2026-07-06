e5fac806fe288504227af60d79194f59002ca46d
aayushiii18 <aayushigupta181@gmail.com>
2026-07-06 10:53:51 +0530

    Fix SQL alias typo in seed.js

--- db/create-admin.js
+++ db/create-admin.js
@@ -2,16 +2,6 @@ const bcrypt = require('bcryptjs');
 const db = require('./database');
 
 const username = 'admin';
-const plainPassword = 'admin123'; // change this to whatever you want
-
-const hash = bcrypt.hashSync(plainPassword, 10);
-db.prepare('INSERT INTO admins (username, password_hash) VALUES (?, ?)').run(username, hash);
-
-console.log(`Admin created: ${username} / ${plainPassword}`);
-const bcrypt = require('bcryptjs');
-const db = require('./database');
-
-const username = 'admin';
 const plainPassword = 'admin123';
 
 const existing = db.prepare('SELECT id FROM admins WHERE username = ?').get(username);
